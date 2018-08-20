e011d7a20faa867bbfcec4de685021ac5a2c8a7b
GZH <guanzh99@gmail.com>
2018-08-20 23:07:03 +0800

    init

---
+++ src/main/java/com/gzh/spring/App.java
@@ -0,0 +1,13 @@
+package com.gzh.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
