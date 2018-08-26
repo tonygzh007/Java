7894c9132c59d948f142f06ea4da716303bf6dee
GZH <guanzh99@gmail.com>
2018-08-26 22:30:00 +0800

    AOP annotation

--- src/main/java/com/springAOP/test.java
+++ src/main/java/com/springAOP/test.java
@@ -4,10 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class test {
-
+    /**
+     * 这个是测试XML文件配置的AOP
+     * @param args
+     */
     public static void main(String[] args){
         ApplicationContext as= new ClassPathXmlApplicationContext("bean.xml");
         ICustomerService cs= (ICustomerService) as.getBean("customerServiceAOP");
         cs.Save();
+        cs.update(20);
+        cs.delete(30);
     }
 }
--- src/main/java/com/springAOP/utils/Logger.java
+++ src/main/java/com/springAOP/utils/Logger.java
@@ -7,4 +7,13 @@ public class Logger {
     public void printLog(){
         System.out.println("Logger中的pringLog方法开始记录日志了。");
     }
+
+    public void closeLog(){
+        System.out.println("Log记录完毕。");
+
+    }
+
+    public void closeParam(){
+        System.out.println("带参数的会执行");
+    }
 }
